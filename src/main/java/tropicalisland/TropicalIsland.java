18f28a5eba982baf30ac7f8c34ce4c0c83b18c2a
Michael Latushko <ancord.m@yandex.ru>
2017-10-09 22:30:16 +0300

    Back tracking draft, Does not working yet.

--- src/main/java/tropicalisland/TropicalIsland.java
+++ src/main/java/tropicalisland/TropicalIsland.java
@@ -3,24 +3,71 @@ package tropicalisland;
 import java.util.Random;
 
 public class TropicalIsland {
-    private static final int H_MAX = 1000;
-    private static final int N_MAX = 50;
-    private static final int M_MAX = 50;
+    private static final int H_MAX = 9;
+    private static final int N_MAX = 5;
+    private static final int M_MAX = 5;
+
+    private Integer[][] W;
 
     public static void main(String[] args) {
         TropicalIsland instance = new TropicalIsland();
         Integer[][] island = instance.getIsland();
+
+        System.out.println("Before rain: ");
+        instance.printIsland(island);
+
+        //instance.initW(island);
         int waterVolume = instance.getWaterVolume(island);
         System.out.println(waterVolume);
     }
 
     public int getWaterVolume(Integer[][] island) {
-        //TODO
+        W = initW(island);
+
+        for(int row = 0; row < island.length; row++){
+            islandWalker(island, 0, row, 0);
+        }
+
+        for(int row = 0; row < island.length; row++){
+            islandWalker(island, island[0].length, row, 0);
+        }
+
+        for(int col = 0; col < island[0].length; col++){
+            islandWalker(island, col, 0, 0);
+        }
+
+        for(int col = 0; col < island[0].length; col++){
+            islandWalker(island, col, island.length, 0);
+        }
+
+        System.out.println("After rain: ");
+        printIsland(W);
+
         return 0;
     }
 
+    void islandWalker(Integer[][] island, int row, int col, int height){
+        if(row < 0 || col < 0 || row >= N_MAX  || col >= M_MAX ){
+            return;
+        }
+
+        if(W[row][col] <= height){
+            return;
+        }
+
+        if(height < island[row][col]){
+            height = island[row][col];
+        }
+        W[row][col] = height;
+
+        islandWalker(island, row-1, col, height);
+        islandWalker(island, row+1, col, height);
+        islandWalker(island, row, col-1, height);
+        islandWalker(island, row, col+1, height);
+    }
+
     //auxiliary methods
-    public void printIsland(Integer[][] island) {
+    void printIsland(Integer[][] island) {
         for (int row = 0; row < island.length; row++) {
             for (int col = 0; col < island[row].length; col++) {
                 System.out.printf("%4d\t", island[row][col]);
@@ -29,12 +76,12 @@ public class TropicalIsland {
         }
     }
 
-    public Integer[][] getIsland() {
+    Integer[][] getIsland() {
         Random random = new Random();
-        //int N = 3;
-        //int M = 5;
-        int N = random.nextInt(N_MAX) + 1;
-        int M = random.nextInt(M_MAX) + 1;
+        int N = 5;
+        int M = 5;
+        //int N = random.nextInt(N_MAX) + 1;
+        //int M = random.nextInt(M_MAX) + 1;
         Integer[][] island = new Integer[N][M];
 
         for(int row = 0; row < N; row++){
@@ -44,5 +91,16 @@ public class TropicalIsland {
         }
 
         return island;
+    }
+
+    Integer[][] initW(Integer[][] island){
+        Integer[][] W = new Integer[island.length][island[0].length];
+        for(int row = 0; row < W.length; row++){
+            for(int col = 0; col < W[row].length; col++){
+                W[row][col] = H_MAX + 1;
+            }
+        }
+
+        return W;
     }
 }
